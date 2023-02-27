41c7bbd388148829ce423d715b41573fb06aa705
ulyana-akinfina <ulyana.akinfina22@gmail.com>
2023-02-27 17:17:37 +0200

    Маршрутизация

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index), # http://127.0.0.1:8000/women/
+    path("", index, name="home"), # http://127.0.0.1:8000/women/
     path('cats/<int:catid>/', categories), #http://127.0.0.1:8000/cats/
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive)
 ]
